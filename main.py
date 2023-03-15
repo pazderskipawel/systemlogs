cba837dd03e81a3f61465bc88677263c9dc65a7d
pazderskipawel <pawelpazderski@hotmail.com>
2023-03-15 19:10:52 +0100

    workers! i think sender don't work (yet)

--- main.py
+++ main.py
@@ -1,28 +1,102 @@
 import threading
+import queue
 import time
-from log_workers import LogWorker, LogSender
-
-logs_queue = LogWorker().logs_queue
-log_worker = LogWorker()
-log_sender = LogSender(logs_queue)
+import subprocess
+import requests
+import platform
 
 
-def stop_workers():
-    log_worker.stop()
-    log_sender.stop()
+class LogWorker(threading.Thread):
+    def __init__(self, num_logs):
+        super().__init__()
+        self.num_logs = num_logs
+        self.stopped = False
 
-    log_worker.join()
-    log_sender.join()
+    def stop(self):
+        self.stopped = True
+
+    def get_windows_logs(self, num_logs):
+        cmd = ['powershell', '-Command', f'Get-WinEvent -LogName System -MaxEvents {num_logs} -Oldest | ConvertTo-Json']
+        output = subprocess.check_output(cmd, encoding='utf-8', errors='replace')
+        logs = output.strip().split('\n')
+        return logs
+
+    def get_linux_logs(self, num_logs):
+        cmd = ['journalctl', '--output=json']
+        output = subprocess.check_output(cmd, encoding='utf-8', errors='replace')
+        logs = output.strip().split('\n')
+        return logs[:num_logs]
+
+    def run(self):
+        while not self.stopped:
+            if platform.system() == 'Windows':
+                logs = self.get_windows_logs(self.num_logs)
+            else:
+                logs = self.get_linux_logs(self.num_logs)
+            print(logs)
+            log_queue.put(logs)
+            # for log in logs:
+            #     print(log)
+            #     log_queue.put(log)
+
+            # Check the stopped flag before sleeping
+            for i in range(10):
+                if self.stopped:
+                    return  # exit the thread
+                time.sleep(1)
 
 
-def main():
-    log_worker.start()
-    log_sender.start()
+class LogSender(threading.Thread):
+    def __init__(self, batch_size, endpoint):
+        super().__init__()
+        self.batch_size = batch_size
+        self.endpoint = endpoint
+        self.stopped = False
 
-    input("Press Enter to stop workers...\n")
+    def stop(self):
+        self.stopped = True
 
-    stop_workers()
+    def run(self):
+        while not self.stopped or not log_queue.empty():
+            batch = []
+            for i in range(self.batch_size):
+                try:
+                    log = log_queue.get(timeout=1)
+                    batch.append(log)
+                except queue.Empty:
+                    break
+
+            if batch:
+                try:
+                    requests.post(self.endpoint, json=batch)
+                    print("Logs sent")
+                except requests.exceptions.RequestException as e:
+                    print(f"Failed to send logs: {e}")
+
+            # Check the stopped flag before sleeping
+            for i in range(10):
+                if self.stopped and log_queue.empty():
+                    return  # exit the thread
+                time.sleep(1)
 
 
 if __name__ == '__main__':
-    main()
+    num_logs = 10
+    batch_size = 5
+    endpoint = "http://localhost:5000/logs"
+
+    log_queue = queue.Queue()
+    log_worker = LogWorker(num_logs)
+    log_sender = LogSender(batch_size, endpoint)
+
+    try:
+        log_worker.start()
+        log_sender.start()
+        input("Press enter to stop...")
+    except KeyboardInterrupt:
+        print("Stopping...")
+    finally:
+        log_worker.stop()
+        log_sender.stop()
+        log_worker.join()
+        log_sender.join()
